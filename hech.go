4dbcd965fdc4c55167477c4e56aed68f8be57803
torbatti <torbattii@gmail.com>
2023-11-03 18:55:30 +0330

    added Xtra Template Maker

--- hech.go
+++ hech.go
@@ -25,3 +25,26 @@ func Min(
 
 	return nil
 }
+
+func X(
+	w http.ResponseWriter,
+	starting_point string,
+	funcMap template.FuncMap,
+	layouts []string,
+	components []string,
+	pages []string,
+	data interface{}) error {
+
+	var filesString []string
+	filesString = append(filesString, layouts...)
+	filesString = append(filesString, components...)
+	filesString = append(filesString, pages...)
+
+	tmpl, err := template.New("").Funcs(funcMap).ParseFiles(filesString[:]...)
+	Check("Template Parsing Error: %v", err)
+
+	err = tmpl.ExecuteTemplate(w, starting_point, data)
+	Check("Template Execution Error: %v", err)
+
+	return nil
+}
